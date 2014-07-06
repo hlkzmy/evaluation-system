1630420f17cf1e6b3be94fa0a8043cf60d849694
hengleike <hengleike.Alfredo>
2014-07-06 13:55:49 +0800

    在网站前台中添加User 和 Evaluate两个控制器 和对应的模板

---
+++ src/Evaluation/WebBundle/Controller/EvaluateController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Evaluation\WebBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class EvaluateController extends Controller
+{
+	public function joinAction()
+    {
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+        return $this->render('EvaluationWebBundle:Evaluate:join.html.twig');
+    }
+}
---
+++ src/Evaluation/WebBundle/Controller/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Evaluation\WebBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class UserController extends Controller
+{
+	
+	
+	public function loginAction()
+    {
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+        return $this->render('EvaluationWebBundle:User:login.html.twig');
+    }
+    
+}
