70a40bbd753282593727a6ad5108be3251bd80e0
hengleike <hengleike.Alfredo>
2014-07-09 13:06:14 +0800

    添加数据库的相关配置 和 指向

---
+++ src/Evaluation/AdminBundle/Controller/BaseController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Evaluation\AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class BaseController extends Controller
+{
+    
+	 function __construct(){
+	 	
+	 	
+	 	
+	 	
+	 	
+	 }//function __construct() end
+    
+    
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
+}
--- src/Evaluation/AdminBundle/Controller/LayoutController.php
+++ src/Evaluation/AdminBundle/Controller/LayoutController.php
@@ -1,13 +1,21 @@
 <?php
 
 namespace Evaluation\AdminBundle\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Evaluation\AdminBundle\Controller\BaseController;
 
-class LayoutController extends Controller
+class LayoutController extends BaseController
 {
     
     public function consoleAction(){
+    	
+    	//第一部分：通过判断session来决定是否调用登录页面
+    	$request = $this->getRequest();
+    	$session = $request->getSession();
+    	if(!$session->has('admin_info')){//如果之前没有登陆过，就跳转到登陆页面
+    		//return $this->redirect($this->generateUrl('evaluation_admin_console'),302);
+    	}
+    	
     	
     	
     	
@@ -19,6 +27,15 @@ class LayoutController extends Controller
     
     public function loginAction(){
     
+    	//第一部分：通过判断session来决定是否调用登录页面
+    	$request = $this->getRequest();
+    	$session = $request->getSession();
+    	
+    	if($session->has('admin_info')){//如果之前登陆过，就直接跳转到后台控制台页面
+    		return $this->redirect($this->generateUrl('evaluation_admin_console'),302);
+    	}
+    	
+
     	
     
     
